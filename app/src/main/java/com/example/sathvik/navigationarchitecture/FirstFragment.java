08a6b25dbc769faac3100372c19766913bb82005
sathvik <sathvik@tatvait.com>
2019-01-02 18:17:02 +0530

    first commit working simple example of android navigation pattern

---
+++ app/src/main/java/com/example/sathvik/navigationarchitecture/FirstFragment.java
@@ -0,0 +1,65 @@
+package com.example.sathvik.navigationarchitecture;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+
+public class FirstFragment extends Fragment {
+
+    Button button;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = view.findViewById(R.id.button_frag1);
+        final NavController navController = Navigation.findNavController(getActivity(), R.id.my_nav_host_fragment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.first_to_second);
+
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
